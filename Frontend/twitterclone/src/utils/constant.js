1b6f70b8b6d31ca0703e868ee08eecfbd7b556c2
krishnapratap98 <117529882+krishnapratap98@users.noreply.github.com>
2024-12-16 01:00:12 +0530

    updatedBackedurl

--- Frontend/twitterclone/src/utils/constant.js
+++ Frontend/twitterclone/src/utils/constant.js
@@ -1,6 +1,7 @@
 // export const USER_API_END_POINT="http://localhost:8080/api/v1/user";
 // export const TWEET_API_END_POINT="http://localhost:8080/api/v1/tweet"
-const backendURL = import.meta.env.VITE_BACKEND_URL;
+
+const backendURL = process.env.REACT_APP_BACKEND_URL;
 console.log(backendURL, 'backendurl');
 export const USER_API_END_POINT=`${backendURL}/api/v1/user`;
 export const TWEET_API_END_POINT=`${backendURL}/api/v1/tweet`;
