07feef284bb240d209c979064bdc62b701eca9f8
krishnapratap98 <117529882+krishnapratap98@users.noreply.github.com>
2024-12-16 01:18:21 +0530

    newUpdate

--- Frontend/twitterclone/src/utils/constant.js
+++ Frontend/twitterclone/src/utils/constant.js
@@ -1,7 +1,7 @@
 // export const USER_API_END_POINT="http://localhost:8080/api/v1/user";
 // export const TWEET_API_END_POINT="http://localhost:8080/api/v1/tweet"
 
-const backendURL = process.env.REACT_APP_BACKEND_URL;
+const backendURL = process.env.REACT_APP_BACKEND_URL || 'https://twitter-fsr5.onrender.com';
 console.log(backendURL, 'backendurl');
 export const USER_API_END_POINT=`${backendURL}/api/v1/user`;
 export const TWEET_API_END_POINT=`${backendURL}/api/v1/tweet`;
