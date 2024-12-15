3288327763457dab4d58591b9c6a4ce583ef5e60
krishnapratap98 <117529882+krishnapratap98@users.noreply.github.com>
2024-12-16 00:40:43 +0530

    changeisdone

--- Frontend/twitterclone/src/utils/constant.js
+++ Frontend/twitterclone/src/utils/constant.js
@@ -1,2 +1,6 @@
-export const USER_API_END_POINT="http://localhost:8080/api/v1/user";
-export const TWEET_API_END_POINT="http://localhost:8080/api/v1/tweet"
+// export const USER_API_END_POINT="http://localhost:8080/api/v1/user";
+// export const TWEET_API_END_POINT="http://localhost:8080/api/v1/tweet"
+const backendURL = import.meta.env.VITE_BACKEND_URL;
+console.log(backendURL, 'backendurl');
+export const USER_API_END_POINT=`${backendURL}/api/v1/user`;
+export const TWEET_API_END_POINT=`${backendURL}/api/v1/tweet`;
